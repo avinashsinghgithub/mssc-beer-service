61048caa03c1b85ea554b204b2d748eb91c8bed0
Avinash Singh <33889911+avinashsinghgithub@users.noreply.github.com>
2022-10-27 23:11:08 +0530

    beer inventory service template addition - part 1

---
+++ src/main/java/guru/springframework/msscbeerservice/services/inventory/BeerInventoryServiceRestTemplateImpl.java
@@ -0,0 +1,47 @@
+package guru.springframework.msscbeerservice.services.inventory;
+
+import guru.springframework.msscbeerservice.web.model.BeerDto;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import java.lang.reflect.Type;
+import java.util.List;
+import java.util.Objects;
+import java.util.UUID;
+
+@Slf4j
+@ConfigurationProperties(prefix = "sfg.brewery", ignoreUnknownFields = false)
+public class BeerInventoryServiceRestTemplateImpl implements BeerInventoryService{
+
+    private final String INVENTORY_PATH = "/api/v1/beer/{beerId}/inventory";
+    private final RestTemplate restTemplate;
+
+    private String beerInventoryServiceHost;
+
+    public void setBeerInventoryServiceHost(String beerInventoryServiceHost){
+        this.beerInventoryServiceHost = beerInventoryServiceHost;
+    }
+    public BeerInventoryServiceRestTemplateImpl(RestTemplateBuilder restTemplateBuilder){
+        this.restTemplate = restTemplateBuilder.build();
+    }
+
+    @Override
+    public Integer getOnHandInventory(UUID beerId) {
+        log.debug("Calling Inventory Service");
+
+        ResponseEntity<List<BeerInventoryDto>> responseEntity =
+                restTemplate.exchange(beerInventoryServiceHost + INVENTORY_PATH, HttpMethod.GET
+                        , null, new ParameterizedTypeReference<List<BeerInventoryDto>>(){}, (Object) beerId);
+
+        Integer onHand = Objects.requireNonNull(responseEntity.getBody())
+                .stream()
+                .mapToInt(BeerInventoryDto::getQuantityOnHand)
+                .sum();
+        return onHand;
+    }
+}
