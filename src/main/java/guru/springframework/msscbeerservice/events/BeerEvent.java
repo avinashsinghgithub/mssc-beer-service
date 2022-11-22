3502740c9fab61dfd066b2af7660c1802d5e1aee
avinash singh <avinash.singh54@gmail.com>
2022-11-22 13:19:06 +0530

    changes to add event related classes

---
+++ src/main/java/guru/springframework/msscbeerservice/events/BeerEvent.java
@@ -0,0 +1,15 @@
+package guru.springframework.msscbeerservice.events;
+
+import guru.springframework.msscbeerservice.web.model.BeerDto;
+import lombok.Builder;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+import java.io.Serializable;
+@Data
+@RequiredArgsConstructor
+@Builder
+public class BeerEvent implements Serializable {
+    static final long serialVersionUID = 474747464646L;
+    private final BeerDto beerDto;
+}
