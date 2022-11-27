d7a750f6edd68120d874cb7b52685544f164f3bd
avinash singh <avinash.singh54@gmail.com>
2022-11-27 20:33:13 +0530

    changes for creating inventory

--- src/main/java/guru/sfg/common/events/NewInventoryEvent.java
+++ src/main/java/guru/sfg/common/events/NewInventoryEvent.java
@@ -1,10 +1,10 @@
-package guru.springframework.msscbeerservice.events;
+package guru.sfg.common.events;
 
 import guru.springframework.msscbeerservice.web.model.BeerDto;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
-public class NewInventoryEvent extends BeerEvent{
+public class NewInventoryEvent extends BeerEvent {
     public NewInventoryEvent(BeerDto beerDto) {
         super(beerDto);
     }
