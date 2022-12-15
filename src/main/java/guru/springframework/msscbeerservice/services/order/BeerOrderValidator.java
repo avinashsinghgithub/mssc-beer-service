65688c2884db413973c5d2c6f9c4439d64569e8b
avinash singh <avinash.singh54@gmail.com>
2022-12-15 14:10:02 +0530

    validate beer order completed

---
+++ src/main/java/guru/springframework/msscbeerservice/services/order/BeerOrderValidator.java
@@ -0,0 +1,25 @@
+package guru.springframework.msscbeerservice.services.order;
+
+import guru.sfg.brewery.events.BeerOrderDto;
+import guru.springframework.msscbeerservice.repositories.BeerRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class BeerOrderValidator {
+    private final BeerRepository beerRepository;
+    public Boolean validateOrder(BeerOrderDto beerOrder){
+        AtomicInteger beerNotFound = new AtomicInteger();
+        beerOrder.getBeerOrderLines().forEach(orderLine ->{
+            if(beerRepository.findByUpc(orderLine.getUpc())== null){
+                beerNotFound.incrementAndGet();
+            }
+        });
+        return beerNotFound.get() == 0;
+    }
+}
