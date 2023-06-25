d7889d725cdb1f1a1c7d664a013a0dc8548d42d6
avinash singh <avinash.singh54@gmail.com>
2023-06-25 19:52:32 +0530

    openfeign added to beer service.

--- src/main/java/guru/springframework/msscbeerservice/services/inventory/BeerInventoryService.java
+++ src/main/java/guru/springframework/msscbeerservice/services/inventory/BeerInventoryService.java
@@ -4,5 +4,6 @@ package guru.springframework.msscbeerservice.services.inventory;
 import java.util.UUID;
 
 public interface BeerInventoryService {
+    String INVENTORY_PATH = "/api/v1/beer/{beerId}/inventory";
     Integer getOnHandInventory(UUID beerId);
 }
--- src/main/java/guru/springframework/msscbeerservice/services/inventory/BeerInventoryServiceRestTemplateImpl.java
+++ src/main/java/guru/springframework/msscbeerservice/services/inventory/BeerInventoryServiceRestTemplateImpl.java
@@ -4,6 +4,7 @@ import guru.springframework.msscbeerservice.services.inventory.model.BeerInvento
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Profile;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
@@ -16,10 +17,11 @@ import java.util.UUID;
 
 @Slf4j
 @Service
+@Profile("!local-discovery")
 @ConfigurationProperties(prefix = "sfg.brewery", ignoreUnknownFields = false)
 public class BeerInventoryServiceRestTemplateImpl implements BeerInventoryService{
 
-    private final String INVENTORY_PATH = "/api/v1/beer/{beerId}/inventory";
+
     private final RestTemplate restTemplate;
 
     private String beerInventoryServiceHost;
