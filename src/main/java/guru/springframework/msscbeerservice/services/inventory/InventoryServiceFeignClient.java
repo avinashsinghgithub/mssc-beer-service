8303dfca8835b0aa7fc3ba05c5792ab2eb8b5037
avinash singh <avinash.singh54@gmail.com>
2023-07-30 17:29:51 +0530

    zipkin configuration and inventory fix

--- src/main/java/guru/springframework/msscbeerservice/services/inventory/InventoryServiceFeignClient.java
+++ src/main/java/guru/springframework/msscbeerservice/services/inventory/InventoryServiceFeignClient.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import java.util.List;
 import java.util.UUID;
 
-@FeignClient(name ="beer-inventory-service",fallback = InventoryServiceFeignClientFailover.class )
+@FeignClient(name ="inventory-service",fallback = InventoryServiceFeignClientFailover.class )
 public interface InventoryServiceFeignClient {
 
     @RequestMapping(method = RequestMethod.GET, value = BeerInventoryService.INVENTORY_PATH)
